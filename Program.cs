9fd7ebc7cc2dbdb2564973b286f827f9bd36f318
santatra <razafindratsimba34@gmail.com>
2024-03-23 19:28:03 +0300

    BUILD: remove Scrutor

--- Program.cs
+++ Program.cs
@@ -2,14 +2,6 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<ShoppingCart.Models.IShoppingCartStore, ShoppingCart.Models.ShoppingCartStore>();
 
-// Using Scrutor for DI
-// builder.Services.Scan(selector =>
-// selector
-// .FromAssemblyOf<Program>()
-// .AddClasses()
-// .AsImplementedInterfaces()
-// );
-
 var app = builder.Build();
 app.UseHttpsRedirection();
 
