02ae5b01867f29fbbd052d4ead54e92dbe06871e
santatra <razafindratsimba34@gmail.com>
2024-03-23 20:48:09 +0300

    REFAC: add comments

--- Models/ShoppingCartStore.cs
+++ Models/ShoppingCartStore.cs
@@ -4,10 +4,11 @@ public class ShoppingCartStore : IShoppingCartStore
     private static readonly Dictionary<int, ShoppingCart>
 	Database = new Dictionary<int, ShoppingCart>();
 
-    // Use an in-memory dictionary instead of a database for now.
+    // Get what's inside a user's cart
     public ShoppingCart Get(int userId) =>
 	Database.ContainsKey(userId) ? Database[userId] : new ShoppingCart(userId);
 
+    // Make a ShoppingCart owned by a User
     public void Save(ShoppingCart shoppingCart) =>
 	Database[shoppingCart.UserId] = shoppingCart;
 
