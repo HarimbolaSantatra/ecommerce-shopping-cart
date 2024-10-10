2fd7f8301fd945186ccba361d4a8720610c43aa3
Santatra <razafindratsimba34@gmail.com>
2024-10-10 22:54:11 +0300

    improve AccountClient: refac, add doc, set port and hostname as environment variable

--- Models/AccountClient.cs
+++ Models/AccountClient.cs
@@ -2,23 +2,44 @@ namespace ShoppingCart.Models;
 
 using Microsoft.AspNetCore.Mvc;
 
+/// <summary>
+/// Class <c>account_test</c> is used as a wrapper for everything around the 'Account' service
+/// </summary>
 public class AccountClient : IAccountClient {
 
-    public readonly HttpClient _client = new HttpClient();
-    public static int AccountPort = 5010;
-    public static string BaseUrl = $"http://127.0.0.1:{AccountPort}";
+    private readonly HttpClient _client = new HttpClient();
+
+    public static string portEnv = Environment.GetEnvironmentVariable("PORT");
+    public static string hostEnv = Environment.GetEnvironmentVariable("HOST");
+
+    private int _accountPort;
+    private string _baseUrl;
+    private string _accountHost;
 
     public AccountClient()
-    {}
+    {
 
-    // Test if the 'Account' service is working
+	if (!Int32.TryParse(portEnv, out _accountPort))
+	{
+	    // Use the default port if cannot read the envVar "PORT"
+	    _accountPort = 5010;
+	}
+
+	_accountHost = String.IsNullOrEmpty(hostEnv) ? "127.0.0.1" : hostEnv;
+	_baseUrl = $"http://{_accountHost}:{_accountPort}";
+
+    }
+
+    /// <summary>
+    /// Test if the 'Account' service is working
+    /// </summary>
     public async Task<ObjectResult> TestService()
     {
 	string response;
 	int statusCode;
 	try
 	{
-	    response = await _client.GetStringAsync($"{BaseUrl}/status");
+	    response = await _client.GetStringAsync($"{_baseUrl}/status");
 	    statusCode = 200;
 	}
 	catch (HttpRequestException e)
@@ -31,13 +52,20 @@ public class AccountClient : IAccountClient {
 	return res;
     }
 
+    /// <summary>
+    /// Get the account of a user
+    /// <param name="userId">The Id of the user</param>
+    /// <returns>
+    /// String
+    /// </returns>
+    /// </summary>
     public async Task<string> GetAccount(int userId)
     {
 	// GET - account/<userId>
 	string jsonResponse;
 	try
 	{
-	    HttpResponseMessage response= await _client.GetAsync($"{BaseUrl}/account/{userId}");
+	    HttpResponseMessage response= await _client.GetAsync($"{_baseUrl}/account/{userId}");
 	    // response.EnsureSuccessStatusCode().WriteRequestToConsole();
 	    jsonResponse = await response.Content.ReadAsStringAsync();
 	    Console.WriteLine($"{jsonResponse}\n");
